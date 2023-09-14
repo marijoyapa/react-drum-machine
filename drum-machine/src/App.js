6e31d4b14eea7a55464d57b1db6cc3b8540e7e4c
marijoyapa <137674934+marijoyapa@users.noreply.github.com>
2023-09-14 11:45:21 +0800

    9 button in three by three columns

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -8,23 +8,32 @@ class Drum extends React.Component {
     this.state = {
 
     }
+    this.playAudio= this.playAudio.bind(this)
+  }
+  playAudio(props){
+    var id = this.props.id
+    console.log(id)
   }
 
   render(){
     return(
       <div className="app">
         <div id="drum-machine">
-          <div id="id-display">
-            <button className="drum-pad" id="Q">Q</button>
-            <button className="drum-pad" id="W">W</button>
-            <button className="drum-pad" id="E">E</button>
-            <button className="drum-pad" id="A">A</button>
-            <button className="drum-pad" id="S">S</button>
-            <button className="drum-pad" id="D">D</button>
-            <button className="drum-pad" id="Z">Z</button>
-            <button className="drum-pad" id="X">X</button>
-            <button className="drum-pad" id="C">C</button>
-          </div>
+          <section id="id-display">
+            <button className="drum-pad" id="Q" onClick={this.playAudio}><audio className= "clip" src='./audio/Heater-1.mp3' id="Q" />Q</button>
+            <button className="drum-pad" id="W"><audio className= "clip" src='./audio/Heater-1.mp3' id="Q" />W</button>
+            <button className="drum-pad" id="E"><audio className= "clip" src='./audio/Heater-1.mp3' id="E" />E</button>
+            <button className="drum-pad" id="A"><audio className= "clip" src='./audio/Heater-1.mp3' id="A" />A</button>
+            <button className="drum-pad" id="S"><audio className= "clip" src='./audio/Heater-1.mp3' id="S" />S</button>
+            <button className="drum-pad" id="D"><audio className= "clip" src='./audio/Heater-1.mp3' id="D" />D</button>
+            <button className="drum-pad" id="Z"><audio className= "clip" src='./audio/Heater-1.mp3' id="Z" />Z</button>
+            <button className="drum-pad" id="X"><audio className= "clip" src='./audio/Heater-1.mp3' id="X" />X</button>
+            <button className="drum-pad" id="C"><audio className= "clip" src='./audio/Heater-1.mp3' id="C" />C</button>
+          </section>
+
+          <section id="configuration">
+
+          </section>
         </div>
 
       </div>
