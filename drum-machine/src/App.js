c7b690b15f3154d16e53791331c12f642c772915
marijoyapa <137674934+marijoyapa@users.noreply.github.com>
2023-09-14 12:34:38 +0800

    Push changes again

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from 'react';
+import audio from './audio/A.mp3'
 
 class Drum extends React.Component {
   constructor(props){
@@ -8,27 +9,38 @@ class Drum extends React.Component {
     this.state = {
 
     }
+    this.audioRefs = {}
     this.playAudio= this.playAudio.bind(this)
   }
-  playAudio(props){
-    var id = this.props.id
-    console.log(id)
+  playAudio = (buttonKey) => {
+    const audio = this.audioRefs[buttonKey];
+    if(audio){
+      audio.currentTime = 0;
+      audio.play();
+    }
   }
 
+  // playAudio(){
+  //   new Audio(audio).play();
+  // }
+
   render(){
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
+
     return(
       <div className="app">
         <div id="drum-machine">
           <section id="id-display">
-            <button className="drum-pad" id="Q" onClick={this.playAudio}><audio className= "clip" src='./audio/Heater-1.mp3' id="Q" />Q</button>
-            <button className="drum-pad" id="W"><audio className= "clip" src='./audio/Heater-1.mp3' id="Q" />W</button>
-            <button className="drum-pad" id="E"><audio className= "clip" src='./audio/Heater-1.mp3' id="E" />E</button>
-            <button className="drum-pad" id="A"><audio className= "clip" src='./audio/Heater-1.mp3' id="A" />A</button>
-            <button className="drum-pad" id="S"><audio className= "clip" src='./audio/Heater-1.mp3' id="S" />S</button>
-            <button className="drum-pad" id="D"><audio className= "clip" src='./audio/Heater-1.mp3' id="D" />D</button>
-            <button className="drum-pad" id="Z"><audio className= "clip" src='./audio/Heater-1.mp3' id="Z" />Z</button>
-            <button className="drum-pad" id="X"><audio className= "clip" src='./audio/Heater-1.mp3' id="X" />X</button>
-            <button className="drum-pad" id="C"><audio className= "clip" src='./audio/Heater-1.mp3' id="C" />C</button>
+          {letters.map((buttonKey) => (
+            <div key={buttonKey} className="button">
+              <button className="drum-pad" onClick={() => this.playAudio(buttonKey)}>{buttonKey}</button>
+              <audio
+                ref={(ref) => (this.audioRefs[buttonKey] = ref)}
+                src={audio}
+                type="audio/mpeg"
+              />
+            </div>
+          ))}
           </section>
 
           <section id="configuration">
@@ -39,6 +51,32 @@ class Drum extends React.Component {
       </div>
     )
   }
+
+  // import audio from './audio/A.mp3'
+  // render(){
+  //   return(
+  //     <div className="app">
+  //       <div id="drum-machine">
+  //         <section id="id-display">
+  //           <button className="drum-pad" id="Q" onClick={this.playAudio}><audio className= "clip" src='./audio/Heater-1.mp3' id="Q" />Q</button>
+  //           <button className="drum-pad" id="W"><audio className= "clip" src='./audio/Heater-1.mp3' id="Q" />W</button>
+  //           <button className="drum-pad" id="E"><audio className= "clip" src='./audio/Heater-1.mp3' id="E" />E</button>
+  //           <button className="drum-pad" id="A"><audio className= "clip" src='./audio/Heater-1.mp3' id="A" />A</button>
+  //           <button className="drum-pad" id="S"><audio className= "clip" src='./audio/Heater-1.mp3' id="S" />S</button>
+  //           <button className="drum-pad" id="D"><audio className= "clip" src='./audio/Heater-1.mp3' id="D" />D</button>
+  //           <button className="drum-pad" id="Z"><audio className= "clip" src='./audio/Heater-1.mp3' id="Z" />Z</button>
+  //           <button className="drum-pad" id="X"><audio className= "clip" src='./audio/Heater-1.mp3' id="X" />X</button>
+  //           <button className="drum-pad" id="C"><audio className= "clip" src='./audio/Heater-1.mp3' id="C" />C</button>
+  //         </section>
+
+  //         <section id="configuration">
+
+  //         </section>
+  //       </div>
+
+  //     </div>
+  //   )
+  // }
 
 
 }
