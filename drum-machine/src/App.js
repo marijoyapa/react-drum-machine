bddd9cf2b689b5289807955c1fb6a2e20ce5d17e
marijoyapa <137674934+marijoyapa@users.noreply.github.com>
2023-09-21 15:00:28 +0800

    Add description of the button played

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,52 +1,88 @@
 import './App.css';
 import React from 'react';
 import audio from './audio/C.mp3'
+const letters = [
+  {
+    "key": 'A',
+    "description": "heater1"
+  },
+  {
+    "key": 'B',
+    "description": "heater2"
+  }, 
+  {
+    "key": 'C',
+    "description": "heater3"
+  }, 
+  {
+    "key": 'D',
+    "description": "heater4"
+  }, 
+  {
+    "key": 'E',
+    "description": "clap"
+  }, 
+  {
+    "key": 'F',
+    "description": "openHH"
+  }, 
+  {
+    "key": 'G',
+    "description": "KicknHat"
+  }, 
+  {
+    "key": 'H',
+    "description": "Kick"
+  }, 
+  {
+    "key": 'I',
+    "description": "ClosedHH"
+  }
+ ];
 
 class Drum extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
 
     this.state = {
-
+      description: ""
     }
     this.audioRefs = {}
-    this.playAudio= this.playAudio.bind(this)
+    this.playAudio = this.playAudio.bind(this)
   }
-  playAudio = (buttonKey) => {
+  playAudio = (buttonKey, description) => {
     // const audio = this.audioRefs[buttonKey];
     const audio = new Audio(require(`./audio/${buttonKey}.mp3`))
-    if(audio){
+    if (audio) {
       audio.currentTime = 0;
       audio.play();
     }
+    this.setState({
+      description
+    })
   }
 
-  // playAudio(){
-  //   new Audio(audio).play();
-  // }
+  render() {
 
-  render(){
-    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'];
-
-    return(
+    return (
       <div className="app">
         <div id="drum-machine">
           <section id="id-display">
-          {letters.map((buttonKey) => (
-            <div key={buttonKey} className="button">
-              <button className="drum-pad" onClick={() => this.playAudio(buttonKey)}>{buttonKey}</button>
-              <audio
-                ref={(ref) => (this.audioRefs[buttonKey] = ref)}
-                src={audio}
-                type="audio/mpeg"
-              />
-            </div>
-          ))}
+            {letters.map((buttonKey) => (
+              <div key={buttonKey.key} className="button">
+                <button className="drum-pad" onClick={() => this.playAudio(buttonKey.key, buttonKey.description)}>{buttonKey.key}</button>
+                <audio
+                  ref={(ref) => (this.audioRefs[buttonKey.key] = ref)}
+                  src={audio}
+                  type="audio/mpeg"
+                />
+              </div>
+            ))}
           </section>
 
           <section id="configuration">
             <div id="power"><label>POWER</label></div>
-            <div id="music-name">MUSIC NAME</div>
+            <div id="music-name">{this.state.description}</div>
             <div id="volume">VOLUME</div>
             <div id="bank">BANK</div>
           </section>
